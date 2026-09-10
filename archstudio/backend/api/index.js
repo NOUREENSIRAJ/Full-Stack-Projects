c8ec1553cf935ae30f04ddbb20a6b364925a05c3
Noureen siraj <noureensiraj30@gmail.com>
2026-09-10 06:24:14 +0500

    Fix DB connection order

--- archstudio/backend/api/index.js
+++ archstudio/backend/api/index.js
@@ -8,20 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const projectRoutes = require('../routes/projectRoutes');
-const contactRoutes = require('../routes/contactRoutes');
-const authRoutes = require('../routes/authRoutes');
-const uploadRoutes = require('../routes/uploadRoutes');
-
-app.use('/api/projects', projectRoutes);
-app.use('/api/contact', contactRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/upload', uploadRoutes);
-
-app.get('/', (req, res) => {
-  res.send('ArchStudio API is running...');
-});
-
 let isConnected = false;
 
 async function connectDB() {
@@ -35,8 +21,23 @@ app.use(async (req, res, next) => {
     await connectDB();
     next();
   } catch (err) {
+    console.log('DB connection error:', err);
     res.status(500).json({ message: 'Database connection failed' });
   }
+});
+
+const projectRoutes = require('../routes/projectRoutes');
+const contactRoutes = require('../routes/contactRoutes');
+const authRoutes = require('../routes/authRoutes');
+const uploadRoutes = require('../routes/uploadRoutes');
+
+app.use('/api/projects', projectRoutes);
+app.use('/api/contact', contactRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/upload', uploadRoutes);
+
+app.get('/', (req, res) => {
+  res.send('ArchStudio API is running...');
 });
 
 module.exports = app;
