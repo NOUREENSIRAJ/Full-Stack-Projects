90205c0a6cef8c8be0b423ca84392785837dddf2
Noureen siraj <noureensiraj30@gmail.com>
2026-09-10 06:42:14 +0500

    Update index.js

--- archstudio/backend/api/index.js
+++ archstudio/backend/api/index.js
@@ -5,7 +5,12 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.options('*', cors());
 app.use(express.json());
 
 let isConnected = false;
